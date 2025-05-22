967c48b80f59d9daafaf409eb5ca77f89932f818
Rishika Singh <125140688+RISHIKA1SINGH@users.noreply.github.com>
2025-05-22 16:35:12 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.css'; // Assuming you are using a separate CSS file for styling
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Welcome to Miilitrack</h1>
+    </div>
+  );
+}
+
+export default Home;
